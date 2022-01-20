6cdac1b831b6996b167ac5c8b200141ae5aa906e
Daniel Ramirez Castano <dm.ramirez@globant.com>
2022-01-20 14:34:17 -0500

    Added necessary config to do integration tests

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MedicoComponent } from './intermediate-two/medico/medico.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MedicosComponent
+    MedicosComponent,
+    MedicoComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/intermediate-two/medico/medico.component.spec.ts
@@ -0,0 +1,24 @@
+import { TestBed, ComponentFixture } from '@angular/core/testing';
+import { MedicoComponent } from './medico.component';
+
+describe('Medico Component', () => {
+
+  let component: MedicoComponent;
+  let fixture: ComponentFixture<MedicoComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [
+        MedicoComponent,
+      ],
+      providers: [],
+      imports: []
+    });
+
+    fixture = TestBed.createComponent(MedicoComponent);
+    component = fixture.componentInstance;
+  });
+
+
+
+});
